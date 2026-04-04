489199f689c08669142c7adf9c0c00e342d25749
Shreyansh Gupta <Shreyanshg2005@gmail.com>
2026-04-04 16:20:15 +0530

    fix(prisma): provide fallback db url

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL || "postgresql://postgres.fnqppnujnaopidfrxqjp:63897Student%402005@aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres?pgbouncer=true"
+    },
+  },
+});
 
 export default prisma;
